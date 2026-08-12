c8d2ad443661be4e34033829c0343fc02db0260e
wendel-ryan <131008174+wendel-ryan@users.noreply.github.com>
2026-08-11 21:06:20 -0500

    Update PublicRoute.jsx

--- frontend/src/components/PublicRoute.jsx
+++ frontend/src/components/PublicRoute.jsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function PublicRoute() {
   const token = localStorage.getItem("access_token");
   // if logged in, go to home; otherwise, show public route
-  return token ? <Navigate to="/home" replace /> : <Outlet />;
+  return token ? <Navigate to="/" replace /> : <Outlet />;
 }
